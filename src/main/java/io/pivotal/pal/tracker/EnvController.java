a5c72cb1866d9d97cee2e7c32938c8e53e18fd08
Ans0912 <anshul8083@gmail.com>
2018-02-21 15:11:15 -0700

    deploying spring boot application to clod foundry

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,13 @@
+package test.pivotal.pal.tracker;
+
+import java.util.Map;
+
+public class EnvController {
+    public EnvController(String s, String s1, String s2, String s3) {
+    }
+
+    public Map<String,String> getEnv() {
+        return null;
+
+    }
+}
