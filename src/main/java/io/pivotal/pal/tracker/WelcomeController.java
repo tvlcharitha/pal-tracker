a5540db8215acc9cd9c928d8aa918af3a8547fd6
venkatrockz <venkateshkandula5@gmail.com>
2018-02-23 15:26:46 -0700

    changes commited

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//my name is welcome controller
     @RestController
     public class WelcomeController {
 
